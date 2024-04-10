676df0ca995b6373f4f277f2caebb1985c1ac75f
ItsJackAnton <jacksa999@hotmail.com>
2024-04-10 11:14:03 -0400

    Window System Refactor
    Before:
    Upon login in, a play button would enable to allow the player to press it and to load into the minigame.
    
    Now:
    
    Upon login in, the game will automatically load you into the minigame.
    The game no longer allow the player to go back to the login menu as this is no longer required.

--- Assets/Plethora/Scripts/Windows/LoginWindow.cs
+++ Assets/Plethora/Scripts/Windows/LoginWindow.cs
@@ -4,47 +4,44 @@ namespace PlethoraV2.UI
     using Boom.UI;
     using PlethoraV2.Addressables;
     using PlethoraV2.Minigames;
-    using System;
-    using UnityEngine;
-    using UnityEngine.UI;
 
     public class LoginWindow : Window
     {
-        [SerializeField] Button playButton;
-
         protected override void Awake()
         {
             base.Awake();
 
-            playButton.onClick.AddListener(OnClickHandler);
+            UserUtil.AddListenerMainDataChange<MainDataTypes.LoginData>(OnLoginStateChange);
         }
         private void OnDestroy()
         {
-            playButton.onClick.RemoveListener(OnClickHandler);
+            UserUtil.RemoveListenerMainDataChange<MainDataTypes.LoginData>(OnLoginStateChange);
         }
-        private void OnClickHandler()
+
+        private void OnLoginStateChange(MainDataTypes.LoginData data)
         {
-            if (ConfigUtil.TryGetConfig(BoomManager.Instance.WORLD_CANISTER_ID, "minigame_1", out MainDataTypes.AllConfigs.Config config) == false)
+            if(data.state == MainDataTypes.LoginData.State.LoggedIn)
             {
-                return;
+                if (ConfigUtil.TryGetConfig(BoomManager.Instance.WORLD_CANISTER_ID, "minigame_1", out MainDataTypes.AllConfigs.Config config) == false)
+                {
+                    return;
+                }
+
+                var minigameConfig = BaseMinigameManager.SetupMinigameConfig(config, "minigame_1", "minigame_won_1");
+
+                BaseMinigameManager.MainConfig_ = minigameConfig;
+
+                ConfigUtil.TryGetConfigFieldAs<string>(BoomManager.Instance.WORLD_CANISTER_ID, minigameConfig.minigameId, "name", out var minigameName, "None");
+                if (string.IsNullOrEmpty(minigameName)) minigameName = "None";
+
+                CoroutineManagerUtil.DelayAction(() =>
+                {
+
+                    SceneManager.Instance.StartSceneTransition(new SceneManager.TransitionArgKey(minigameConfig.sceneName));
+                    Close();
+
+                }, 2f, transform);
             }
-
-            playButton.interactable = false;
-
-            var minigameConfig = BaseMinigameManager.SetupMinigameConfig(config, "minigame_1", "minigame_won_1");
-
-            BaseMinigameManager.MainConfig_ = minigameConfig;
-
-            ConfigUtil.TryGetConfigFieldAs<string>(BoomManager.Instance.WORLD_CANISTER_ID, minigameConfig.minigameId, "name", out var minigameName, "None");
-            if (string.IsNullOrEmpty(minigameName)) minigameName = "None";
-
-            CoroutineManagerUtil.DelayAction(() =>
-            {
-
-                SceneManager.Instance.StartSceneTransition(new SceneManager.TransitionArgKey(minigameConfig.sceneName));
-                Close();
-
-            }, 2f, transform);
         }
 
         public override bool RequireUnlockCursor()
